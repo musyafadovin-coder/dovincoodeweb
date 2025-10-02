611381978d548c8791c7c17e5a127709c8bcb22e
musyafadovin-coder <musyafadovin@gmail.com>
2025-10-02 12:30:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementByid('sapaButton');
+const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click',function(){
   alert('halo! terima kasih sudah berkunjung!');
 });
